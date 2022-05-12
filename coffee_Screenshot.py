c1ad991319e81061cd38297c6716e07d4940e901
raisayon <59853083+raisayon@users.noreply.github.com>
2022-05-12 18:40:08 +0800

    replaced python dictionary with json

--- coffee_Screenshot.py
+++ coffee_Screenshot.py
@@ -19,54 +19,54 @@ import json
 #time.sleep(5) # Let the user actually see something!
 
 #dictionary of the name and address it belongs to 
+x = {
+   "wang": ["f01111881","f01464670","f01154375","f01034007","f01501599","f01589943"],
+   "shen": ["f01054527","f01177326"],
+   "mark":["f01265322","f01137193"],
+   "chen":["f01464400","f01372569","f01597362","f01445415","f01698865","f01771575"],
+   "ben":["f01264903"],
+   "deng":["f01316365"],
+   "coffeecloud":["f01807413","f01822659"],
+   "mei":["f01624906","f01782079","f01831595"],
+   "ivan":["f01727648","f01768764"]
 
-address_dic = {
-    #"wang":"f01111881",
-    #"wang1":"f01464670",
-    #"wang2":"f01154375",
-    #"wang3":"f01034007",
-    #"wang4":"f01501599","wang5":"f01589943",
-    #"shen":"f01054527","shen1":"f01177326",
-    #"mark":["f01265322"],
-    "chen":['f01464400','f01372569','f01597362','f01445415','f01698865','f01771575'],
-    "ben":['f01264903'],
-    "deng":["f01316365"],
-    "coffeecloud":["f01807413"],
-    "su":["f01624906","f01782079"],
-    "rollingstone":["f01626808","f01716971"],
-    "Mr.li":["f01700446"],
-    "ivan":["f01727648","f01768764"]
-   
-    }
+}
+
+y = json.dumps(x)
+data = json.loads(y)
+address_dic = data
 
 def screenshot_filfox(address):
     #address_input = input("Enter your address")
+    #tala ko line ma chai pass garnu parne 
     print(address.keys())
     for name in address.keys():
         #os.mkdir(f'/Users/sayonrai/CC/dailydata/{name}')
-        for address in address[name]:
+        for addr in address[name]:
             url = "https://filfox.info/en/"
             driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
             driver.get(url) # website link
             driver.implicitly_wait(3)
             driver.execute_script("document.body.style.zoom='50%'") #website zoom 
-            print(f'{name} {address}')
+            print(f'{name} {addr}')
             time.sleep(5)
             search_box = driver.find_element_by_xpath('//*[@id="__layout"]/div/nav/div[2]/div[5]/input')
-            search_box.send_keys(address)
+            search_box.send_keys(addr)
+            #for loop chalako pass vaye ni number matra hunxa 'f01111881' pass hudaina
             search_box.send_keys(Keys.RETURN)
             time.sleep(5) # Let the user actually see something!
             #takes screenshot
-            image = ss.full_Screenshot(driver, save_path= f'/Users/sayonrai/CC/dailydata/{name}', image_name=f'{address}.png')
+            image = ss.full_Screenshot(driver, save_path= f'/Users/sayonrai/CC/dailydata/{name}', image_name=f'{addr}.png')
             #screen = Image.open(image)
             #screen.show()
             
             #refresh
             driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'r')
             time.sleep(3)
-            #click address link
-            #owner_address = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[1]/div/div[6]/div/div[2]/div[2]/a')
-            #owner_address.click()
+            """
+            owner_address = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[1]/div/div[6]/div/div[2]/div[2]/a')
+            owner_address.click()
+            """
 
 
 screenshot_filfox(address_dic)
